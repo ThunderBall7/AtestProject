cf74aa27a3198c0786a5388f05924d8982e5c2ab
Manish <manishmaurya.2204@gmail.com>
2024-02-08 17:33:16 +0530

    some changes

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -42,7 +42,7 @@ const Product = ({ data, loading, error, handleIsloggedIn }) => {
     return <div>Error: {error}</div>;
   }
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 w-[95%] mx-auto">
       {data.map((elem, index) => (
         <div key={elem.id || index} className="flex w-96 mx-auto h-[120px] border border-blue-600 bg-white rounded-lg">
           <img src={elem.src} alt={elem.heading} className="w-32 h-auto mr-4 object-cover rounded-lg" />
